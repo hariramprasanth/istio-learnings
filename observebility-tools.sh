32116205d6d3eb1dded107294779c4f291859239
hariramprasanth <hariramprasanth0@gmail.com>
2023-05-11 12:16:05 +0530

    Add. observebility tools

---
+++ observebility-tools.sh
@@ -0,0 +1,5 @@
+#! /bin/zsh
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.14/samples/addons/jaeger.yaml 
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.14/samples/addons/prometheus.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.14/samples/addons/grafana.yaml 
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.14/samples/addons/kiali.yaml
